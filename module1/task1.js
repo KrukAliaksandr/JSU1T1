4464509e1c684fd9061391c67c89072d3defeb70
Aliaksandr_Kruk <Aliaksandr_Kruk@epam.com>
2018-11-19 07:34:51 +0300

    refactored

--- module1/task1.js
+++ module1/task1.js
@@ -1,32 +1,38 @@
 /* eslint-disable no-console */
 /* eslint-disable indent */
 
+const fs = require("fs");
 
-function displayFileEvenLines(path, encoding) {
-    checkParametres(path, encoding);
-    let fs = require("fs");
-    fs.readFile(path, encoding, function (err, contents) {
-        if (err) {
-            console.log(err.message);
-        }
-        console.log(contents);
-        let fileStrings = contents.split("\r\n");
-        if (fileStrings.length < 2) {
-            throw new Error("File must have at least 2 lines");
-        }
-        else {
-            for (let i = 0; i < fileStrings.length(); i + 2) {
-                console.log(fileStrings[i]);
-            }
-        }
+const path = "textdummy.txt";
+const encoding = "utf8";
+
+function readFile(filename, enc) {
+    return new Promise(function (resolve, reject) {
+        fs.readFile(filename, enc, function (err, res) {
+            if (err) reject(err);
+            else resolve(res);
+        });
     });
-    console.log("after calling readFile");
 }
 
-function checkParametres(pathToFile, encoding) {
-    if (typeof pathToFile !== "string" || typeof encoding !== "string") {
-        throw new Error("Wrong type of variable. Path to file or encoding must be String");
+function printEvenLines(fileStrings) {
+    if (fileStrings.length < 2) {
+        throw new Error("File must have at least 2 lines");
+    }
+    else {
+        fileStrings = fileStrings.filter(function (item, index) {
+            return (index % 2 !== 0);
+        });
+        console.log(fileStrings);
     }
 }
 
-displayFileEvenLines("textdummy.txt", "utf8");
+let result = function readFileEvenLines(path, encoding) {
+    readFile(path, encoding).then((readResult) => {
+        let fileStrings = readResult.split("\r\n");
+        printEvenLines(fileStrings);
+    })
+        .catch((error) => { console.log(error.message); });
+};
+
+result(path, encoding);
